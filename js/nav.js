b6596b9f425338291e33e388d8b78447edf628d0
eprochnau <proc0035@algonquinlive.com>
2014-01-06 10:07:17 -0500

    Uploaded files.

---
+++ js/nav.js
@@ -0,0 +1,34 @@
+// Toggle Nav JS
+
+	var navTop = document.querySelector('.nav-top');
+	
+	document.querySelector('.nav-btn').addEventListener('click', function(e){
+		e.preventDefault(); 
+	
+	
+		if (navTop.getAttribute('data-state') == 'expanded'){
+			
+			navTop.setAttribute('data-state', 'collapsed');
+		  this.setAttribute('data-state', 'inactive');
+		}else{
+		
+			navTop.setAttribute('data-state', 'expanded');
+	    this.setAttribute('data-state', 'active');
+		}
+	});
+	
+<script type="text/javascript">
+	window.onload=function(){
+	var tfrow=document.getElementById('tfhover').rows.length;
+	var tbRow=[];
+	for (var i=1;i<tfrow;i++) {
+		tbRow[i]=document.getElementById('tfhover').rows[i];
+		tbRow[i].onmouseover = function(){
+		  this.style.backgroundColor = '#ffffff';
+		};
+		tbRow[i].onmouseout = function() {
+		  this.style.backgroundColor = '#d4e3e5';
+		};
+	}
+};
+</script>
